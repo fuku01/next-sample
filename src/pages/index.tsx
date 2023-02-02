17e68295c150d62415886310963e25d440b0d315
fuku <>
2023-02-02 23:34:28 +0900

    複数のボタンコンポーネントを一つに統合 ボタンを押すたびに、特定の文字列が表示・非表示されるようにuseState(の処理を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import { Button } from '@/components/Button'
+import { useState } from 'react'
 
 const Home = () => {
+  const [first, setFirst] = useState(false)
   return (
-    <>
-      <div className="text-3xl font-bold text-blue-500">Heloo!!!!!</div>
-      <p>animalanimal</p>
-      <div>aaa</div>
-      <Button name={"ボタン"} />
-    </>
+    <div className="h-screen text-center bg-gray-300">
+      <div className="text-3xl font-bold text-blue-500 pt-10">Hello!!!!!</div>
+      <div onClick={() => setFirst(!first)}>
+        <Button name={"ボタン"} color={"red"} />
+      </div>
+      {first === true && <div>クリックされました</div>}
+    </div>
   )
 }
+
 export default Home
