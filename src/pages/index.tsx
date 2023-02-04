12519f5ea1923d69be737ed47b33390ef59604cb
fuku <77525559+fuku01@users.noreply.github.com>
2023-02-05 00:02:11 +0900

    Usernameフォームの追加、Useeffectを使用した文字の表示・非表示処理を追加 (#4)
    Co-authored-by: fuku <>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,35 @@
 import { Button } from '@/components/Button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Textarea } from "@material-tailwind/react";
+import { Input } from "@material-tailwind/react";
 
 const Home = () => {
-  const [message, setMessage] = useState("")
   const [click, setClick] = useState(false)
+
+  const [message, setMessage] = useState("")
+  const [displayMessage, setDisplayMessage] = useState("")
+
+  const [username, setUsername] = useState("")
+  const [displayUsername, setDisplayUsername] = useState("")
+
+  useEffect(() => {
+    if (click === true) {
+      setDisplayUsername(username)
+      setDisplayMessage(message)
+    }
+    else {
+      setDisplayUsername("")
+      setDisplayMessage("")
+    }
+  }, [click])
+
   return (
     <div className="h-screen text-center bg-gray-300">
       <div className="text-3xl font-bold text-blue-500 pt-10">Hello!!!!!</div>
+      <div className="w-96 mx-auto mb-8">
+        <Input label="Username" onChange={(c) =>
+          setUsername(c.target.value)} />
+      </div>
       <div className="w-96 mx-auto">
         <Textarea label="Message" onChange={(e) =>
           setMessage(e.target.value)} />
@@ -15,7 +37,8 @@ const Home = () => {
       <div onClick={() => setClick(!click)}>
         <Button name={"ボタン"} />
       </div>
-      {click === true && <div>{message}</div>}
+      {click === true && <div>{displayUsername}</div>}
+      {click === true && <div>{displayMessage}</div>}
     </div>
   )
 }
