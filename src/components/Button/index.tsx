989c2c25d5f76d484f85f2b54f898541fcb7d1a9
fuku <77525559+fuku01@users.noreply.github.com>
2023-02-08 23:22:35 +0900

    表示データの位置調整、チェックボックスと削除ボタンの追加 (#6)
    * 表示データの位置調整、チェックボックスと削除ボタンの追加
    
    * 削除ボタンを押した要素を配列から削除する処理を追加
    
    ---------
    
    Co-authored-by: fuku <>

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,24 @@
 type ButtonProps = {
     name: string
 }
+type RedButtonProps = {
+    name: string
+}
 
 export const Button = (props: ButtonProps) => {
     return (
         <button
             className={"py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75 w-28 mx-auto"}
+        >
+            {props.name}
+        </button>
+    )
+}
+
+export const RedButton = (props: RedButtonProps) => {
+    return (
+        <button
+            className={"py-1 px-1 bg-red-500 text-white text-xs rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75 w-10 mx-auto"}
         >
             {props.name}
         </button>
