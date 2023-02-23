580f10bbdf80533e56442c17bfb3db21777e59a1
sako0 <y.m1911a1.0921@gmail.com>
2023-02-23 20:45:13 +0900

    todoの削除機能、追加機能を追加した

--- src/pages/hello/index.tsx
+++ src/pages/hello/index.tsx
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { Button, RedButton } from "@/components/Button"
-import { Input } from "@material-tailwind/react"
+import { Button, RedButton } from "@/components/Button";
+import { Input } from "@material-tailwind/react";
 import { useEffect, useState } from "react";
 import { Checkbox } from "@material-tailwind/react";
 // サーバーとのやり取りをするためのライブラリ
@@ -9,33 +9,23 @@ import axios from "axios";
 
 // 事前に型を定義しておく
 type Todo = {
-  id: number
-  text: string
-  isChecked: boolean
-}
+  id: number;
+  text: string;
+  isChecked: boolean;
+};
 
+type TodoResponse = {
+  ID: number;
+  text: string;
+};
 
 const Hello = () => {
-
   // さこが作ったAPI
-  const baseURL = "http://localhost:5003/todos";
-
-  // データベースに入っている配列をAPIで持ってきて、リストを初期表示させるための処理
-  useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      const getList = response.data.map((item: { text: string }, index: number) => ({
-        id: index,
-        text: item.text,
-        isChecked: false
-      }));
-      setTodoList(getList);
-    });
-  }, []);
+  const baseURL = "http://localhost:5003";
 
   // useState(変数的なやつ)
-  const [typing, setTyping] = useState<string>("")
-  const [todoList, setTodoList] = useState<Todo[]>([])
-
+  const [typing, setTyping] = useState<string>("");
+  const [todoList, setTodoList] = useState<Todo[]>([]);
 
   // id を指定するとCheckされる関数
   const checkTodo = (id: number) => {
@@ -49,21 +39,44 @@ const Hello = () => {
     // newTodosをstateに入れる
     setTodoList(newTodos);
   };
+  // データベースに入っている配列をAPIで持ってきて、リストを初期表示させるための処理
+  const getTodoList = () => {
+    axios.get(baseURL + "/todos").then((response) => {
+      const getList: Todo[] = response.data.map((item: TodoResponse) => {
+        return {
+          id: item.ID,
+          text: item.text,
+          isChecked: false,
+        };
+      });
+
+      setTodoList(getList);
+    });
+  };
+
+  // 特定のidをtodolistから削除する関数
+  const postTodoList = (text: string) => {
+    axios.post(baseURL + "/todo", { text: text }).then(() => {
+      getTodoList();
+    });
+  };
 
   // 特定のidをtodolistから削除する関数
   const deleteTodoList = (id: number) => {
-    const newTodoList = Object.assign([], todoList)
-    const todoFind = (todo: Todo) => {
-      return todo.id !== id
-    }
-    const result = newTodoList.filter(todoFind)
-    setTodoList(result)
-  }
+    axios.delete(baseURL + "/todo/" + id).then(() => {
+      getTodoList();
+    });
+  };
+
+  // データベースに入っている配列をAPIで持ってきて、リストを初期表示させるための処理
+  useEffect(() => {
+    getTodoList();
+  }, []);
 
   // コンソールログで配列の中身確認する用
   useEffect(() => {
     console.log(todoList);
-  }, [todoList])
+  }, [todoList]);
 
   return (
     // タイトル表示
@@ -73,65 +86,74 @@ const Hello = () => {
       {/* 入力フォーム */}
       <div className="flex justify-center">
         <div className="w-96 mr-5 bg-white rounded-md">
-          <Input color="green" label="Add New Task"
-            onChange={(e) =>     //入力した最新の文字列をtypingへ入れる
-              setTyping(e.target.value)} />
+          <Input
+            color="green"
+            label="Add New Task"
+            onChange={(
+              e //入力した最新の文字列をtypingへ入れる
+            ) => setTyping(e.target.value)}
+          />
         </div>
 
         {/* 追加ボタン */}
-        <div onClick={() => {
-          const newTodoList = Object.assign([], todoList) //stateの配列データ(todoList)を、純粋な配列データ(newTodoList)にする
-          {
-            typing === ("") ? alert("タスクを入力してください！") :
-              newTodoList.push({                     //pushで、newTodoListの配列データに各オブジェクトを追加する（オブジェクトの型はTodoで指定）
-                id: newTodoList.length,
-                text: typing,
-                isChecked: false
-              })
-            setTodoList(newTodoList)
-          }
-        }} >
+        <div
+          onClick={() => {
+            postTodoList(typing);
+          }}
+        >
           <Button name={"追加"} />
         </div>
       </div>
 
       {/* //白いエリアを作成して、それを中央寄せしてレイアウトを整える*/}
       <div className="w-1/3 mx-auto mt-8 bg-white rounded-md divide-y divide-gray-400 divide-dashed">
-
         {/* todoリストの表示 */}
-        {
-          todoList.map((value) => {
-            return (
-              <div key={value.id} className="flex items-center">
-
-                {/* チェックボタン */}
-                <div onClick={() => {
-                  checkTodo(value.id)
+        {todoList.map((value) => {
+          return (
+            <div key={value.id} className="flex items-center">
+              {/* チェックボタン */}
+              <div
+                onClick={() => {
+                  checkTodo(value.id);
                 }}
-                >
-                  <Checkbox color="green" defaultChecked={value.isChecked} className="text-left" />
-                </div>
+              >
+                <Checkbox
+                  id={String(value.id)}
+                  checked={value.isChecked}
+                  color="green"
+                  defaultChecked={false}
+                  className="text-left"
+                />
+              </div>
 
-                {/* 表示されるtodoリストのチェック有無による色分け */}
-                <div className={value.isChecked ? "text-gray-500 line-through" : "text-black-500"}>
-                  {value.text}</div>
-
-                {/* チェックされているものだけ削除ボタンを表示する */}
-                {value.isChecked &&
-                  <div onClick={() => {
-                    deleteTodoList(value.id)
-                  }}
-                    className="ml-auto mr-3" >
-                    <RedButton name={"削除"} />
-                  </div>
+              {/* 表示されるtodoリストのチェック有無による色分け */}
+              <div
+                className={
+                  value.isChecked
+                    ? "text-gray-500 line-through"
+                    : "text-black-500"
                 }
-              </div >
-            )
-          })
-        }
-      </div>
-    </div >
-  )
-}
+              >
+                {value.text}
+              </div>
 
-export default Hello
+              {/* チェックされているものだけ削除ボタンを表示する */}
+              {value.isChecked && (
+                <div
+                  onClick={() => {
+                    deleteTodoList(value.id);
+                  }}
+                  className="ml-auto mr-3"
+                >
+                  <RedButton name={"削除"} />
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Hello;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,38 @@
-import { Button } from '@/components/Button'
-import { useEffect, useState } from 'react'
+import { Button } from "@/components/Button";
+import { useEffect, useState } from "react";
 import { Textarea } from "@material-tailwind/react";
 import { Input } from "@material-tailwind/react";
 
 const Home = () => {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
 
-  const [message, setMessage] = useState("")
-  const [displayMessage, setDisplayMessage] = useState("")
+  const [message, setMessage] = useState("");
+  const [displayMessage, setDisplayMessage] = useState("");
 
-  const [username, setUsername] = useState("")
-  const [displayUsername, setDisplayUsername] = useState("")
+  const [username, setUsername] = useState("");
+  const [displayUsername, setDisplayUsername] = useState("");
 
   useEffect(() => {
     if (click === true) {
-      setDisplayUsername(username)
-      setDisplayMessage(message)
+      setDisplayUsername(username);
+      setDisplayMessage(message);
+    } else {
+      setDisplayUsername("");
+      setDisplayMessage("");
     }
-    else {
-      setDisplayUsername("")
-      setDisplayMessage("")
-    }
-  }, [click])
+  }, [click]);
 
   return (
     <div className="h-screen text-center bg-gray-300">
       <div className="text-3xl font-bold text-blue-500 pt-10">Hello!!!!!</div>
       <div className="w-96 mx-auto mb-8">
-        <Input label="Username" onChange={(c) =>
-          setUsername(c.target.value)} />
+        <Input label="Username" onChange={(c) => setUsername(c.target.value)} />
       </div>
       <div className="w-96 mx-auto">
-        <Textarea label="Message" onChange={(e) =>
-          setMessage(e.target.value)} />
+        <Textarea
+          label="Message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
       </div>
       <div onClick={() => setClick(!click)}>
         <Button name={"ボタン"} />
@@ -40,7 +40,7 @@ const Home = () => {
       {click === true && <div>{displayUsername}</div>}
       {click === true && <div>{displayMessage}</div>}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
